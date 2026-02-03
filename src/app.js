72afce885e53bd1b1df1bba619636147374fabd3
JLM21-JinkaLM <maneeshajl21@gmail.com>
2026-02-03 12:31:11 +0530

    commited

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const productRoutes = require("./routes/productRoutes")
+const orderRoutes = require("./routes/orderRoutes")
+const userRoutes = require("./routes/userRoutes")
+
+const authRoutes = require('./routes/authRoutes')
+
+const app=express()
+
+app.use(express.json())
+
+app.use("/api/products",productRoutes)
+app.use("/api/orders",orderRoutes)
+app.use("/api/users",userRoutes)
+app.use("api/auth", authRoutes)
+
+module.exports = app;
