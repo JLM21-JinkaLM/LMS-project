9af984a3e0d8ff38270b346058d0f56e82727b55
JLM21-JinkaLM <maneeshajl21@gmail.com>
2026-02-03 14:29:22 +0530

    commited

--- src/app.js
+++ src/app.js
@@ -13,6 +13,6 @@ app.use(express.json())
 app.use("/api/products",productRoutes)
 app.use("/api/orders",orderRoutes)
 app.use("/api/users",userRoutes)
-app.use("api/auth", authRoutes)
+app.use("/api/auth", authRoutes)
 
 module.exports = app;
