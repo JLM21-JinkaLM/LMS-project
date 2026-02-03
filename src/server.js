601f84eb51c635509f682e30a791580514c88b2f
JLM21-JinkaLM <maneeshajl21@gmail.com>
2026-02-03 12:33:32 +0530

    commited

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const app=require('./app')
+
+const connectToDb = require('./config/db');
+
+connectToDb();
+
+app.listen(process.env.PORT,()=>{
+    console.log('server running')
+})
